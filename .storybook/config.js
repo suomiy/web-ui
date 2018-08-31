95df35e7cf538f3216b4d3842f8ff823704f0536
suomiy <suomiy@gmail.com>
2018-08-31 14:30:12 +0200

    add storybook deployer + conf

---
+++ .storybook/config.js
@@ -0,0 +1,18 @@
+import { configure } from '@storybook/react';
+import { setOptions } from '@storybook/addon-options';
+import 'patternfly-react/dist/css/patternfly-react.css';
+import 'patternfly/dist/css/patternfly.css'
+import 'patternfly/dist/css/patternfly-additions.css'
+
+const storyContext = require.context('../src', true, /\.stories\.js$/);
+
+function loadStories() {
+  storyContext.keys().forEach(storyContext);
+}
+
+setOptions({
+  name: 'kubevirt-web-ui',
+  sortStoriesByKind: true
+});
+
+configure(loadStories, module);
